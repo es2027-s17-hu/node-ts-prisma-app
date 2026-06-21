d57edd241df5a37d4d13f00a3f08d69046f179bd
Zoltan Sisak <zoltan.sisak@gmail.com>
2026-06-22 00:31:21 +0900

    Refactor project ts to js

--- src/prisma.js
+++ src/prisma.js
@@ -2,8 +2,7 @@ import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "./generated/prisma/client.js";
 
-
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
